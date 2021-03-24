4d7c6d5b56a1b3a3664539c2c9b2e35961fe2097
daniel30-07 <danielborgesdecarvalho@gmail.com>
2021-03-24 19:54:35 -0300

    Saving data to form

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -14,11 +14,24 @@ export function NewTransactionModal({
   isOpen,
   onRequestClose,
 }: NewTransactionModalProps) {
+  const [title, setTitle] = useState("");
+  const [value, setValue] = useState(0);
+  const [category, setCategory] = useState("");
+
   const [type, setType] = useState("deposit");
   const green = "#33cc954d";
   const red = "#e52e4d4d";
 
-  function handleCreateNewTransaction(event: FormEvent) {}
+  function handleCreateNewTransaction(event: FormEvent) {
+    event.preventDefault();
+
+    const data = {
+      title,
+      value,
+      category,
+      type,
+    };
+  }
 
   return (
     <Modal
@@ -36,9 +49,18 @@ export function NewTransactionModal({
       </button>
       <Container onSubmit={handleCreateNewTransaction}>
         <h2>Register New Transaction</h2>
-        <input placeholder="Title" />
+        <input
+          placeholder="Title"
+          value={title}
+          onChange={(event) => setTitle(event.target.value)}
+        />
 
-        <input type="number" placeholder="Value" />
+        <input
+          type="number"
+          placeholder="Value"
+          value={value}
+          onChange={(event) => setValue(Number(event.target.value))}
+        />
 
         <TransactionTypeContainer>
           <ButtonType
@@ -65,7 +87,11 @@ export function NewTransactionModal({
           </ButtonType>
         </TransactionTypeContainer>
 
-        <input placeholder="Category" />
+        <input
+          placeholder="Category"
+          value={category}
+          onChange={(event) => setCategory(event.target.value)}
+        />
 
         <button type="submit">Register</button>
       </Container>
