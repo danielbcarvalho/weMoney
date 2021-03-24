586ecb75432489c5cac3dc554fa224b74992cbdc
daniel30-07 <danielborgesdecarvalho@gmail.com>
2021-03-24 13:31:50 -0300

    Add New Transaction Modal component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,27 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import Modal from "react-modal";
 import logoImg from "../../assets/LogoWeMoney2.svg";
 import { Container, Content } from "./styles";
+interface HeaderProps {
+  onOpenNewTransactionModal: () => void;
+}
 
-const customStyles = {
-  content: {
-    top: "50%",
-    left: "50%",
-    right: "auto",
-    bottom: "auto",
-    marginRight: "-50%",
-    transform: "translate(-50%, -50%)",
-  },
-};
-
-Modal.setAppElement("#root");
-
-export function Header() {
+export function Header({ onOpenNewTransactionModal }: HeaderProps) {
   return (
     <Container>
       <Content>
         <img src={logoImg} alt="WeMoney" />
-        <button type="button">New Transaction</button>
+        <button type="button" onClick={onOpenNewTransactionModal}>
+          New Transaction
+        </button>
       </Content>
     </Container>
   );
